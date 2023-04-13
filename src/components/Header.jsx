cacbc5676e3a2a0d52fddc27ddefd22d300a1b8e
EFTEKHER <eftekherali2000@gmail.com>
2023-04-13 20:33:36 +0600

    components

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import Footer from "./Footer.js";
+
+function Header() {
+  return (
+    <div>
+      <h1>This is header</h1>
+      <p>This is an amazing tutorial</p>
+      <Footer />
+    </div>
+  );
+}
+
+export default Header;
+export { Footer };
