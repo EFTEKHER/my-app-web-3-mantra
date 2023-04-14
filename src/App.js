03dd434315cb71e6c95b41f28bafd1d9b32a4ee0
EFTEKHER <eftekherali2000@gmail.com>
2023-04-15 00:48:03 +0600

    if-else

--- src/App.js
+++ src/App.js
@@ -3,9 +3,22 @@ import Header from "./components/Header";
 import Movie from "./components/Movie";
 import movies from './data.json';
 function App() {
-
+let login=true;
   return (
+    
     <div className="App">
+
+    {
+      (()=>{
+      if(login)
+      {
+        return <h1 style={{color:'black'}}>see this </h1>;
+      }
+      else{
+        return <h1 style={{color:'pink'}}>Not see this</h1>
+      }
+      })()
+    }
   <Header/>
 <div className="main">
 {
