56d8234ca794464e2bd89bb28ad3784a7c7c8fa2
EFTEKHER <eftekherali2000@gmail.com>
2023-04-14 16:50:59 +0600

    styling-component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 import Footer from "./Footer.js";
-
+import "./header.css";
 function Header() {
   return (
-    <div>
-      <h1>This is header</h1>
+    <div style={{backgroundColor:"pink"}} className="header">
+      <h1 id="topic">This is header</h1>
       <p>This is an amazing tutorial</p>
-      <Footer />
+    
     </div>
   );
 }
